3e4ea3c1435ce9225219208babcf17cd932a41e7
Petrov-V-V <vp6514764@gmail.com>
2023-07-08 23:05:18 +0300

    delete backend

--- backend/src/main/java/ru/sber/project_todo_list/entities/ERole.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.project_todo_list.entities;
-
-/**
- * Enumerate отвечающий за хранение возможных ролей
- */
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- backend/src/main/resources/schema.sql
+++
@@ -1,1 +0,0 @@
-create schema if not exists todo_list_petrov_vv;
